1b9dd0538704cfc5e7dc286d72e17eb2c1e9ed62
sumanthlearningaem3320 <saisumanthguntur@gmail.com>
2024-02-13 21:18:22 +0530

    test6

--- print.sh
+++ print.sh
@@ -2,6 +2,6 @@ echo helloworld
 
 ## echo -e "[\COLm message \e[0m"
 
-echo -e "[\32m this issomecolour \e[0m"
+echo -e "\e[32m this issomecolour \e[0m"
 
-echo -e "[\31m this redissomecolour \e[0m"
+echo -e "\e[\31m this redissomecolour \e[0m"
